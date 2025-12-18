460a43d3f4788a56580e79355b52ff175263e829
pankaj-repo <pankaj_kumar9060@yahoo.com>
2025-12-19 01:18:51 +0530

    sound fixed

--- src/config/audio.js
+++ src/config/audio.js
@@ -2,18 +2,18 @@ import { Howl } from 'howler';
 
 export const sfx = {
     // Common Sounds
-    pop: new Howl({ src: ['/sounds/pop/bubble-pop.mp3'], volume: 0.5 }),
-    win: new Howl({ src: ['/sounds/win/brass-fanfare-with-timpani-and-winchimes-reverberated.mp3'], volume: 0.6 }),
-    switch: new Howl({ src: ['/sounds/pop/bubble-pop.mp3'], volume: 0.5 }),
+    pop: new Howl({ src: ['sounds/pop/bubble-pop.mp3'], volume: 0.5 }),
+    win: new Howl({ src: ['sounds/win/brass-fanfare-with-timpani-and-winchimes-reverberated.mp3'], volume: 0.6 }),
+    switch: new Howl({ src: ['sounds/pop/bubble-pop.mp3'], volume: 0.5 }),
 
     // Game Specific Sounds
     bg: new Howl({
-        src: ['/sounds/background/music-for-game-fun-kid-game.mp3'],
+        src: ['sounds/background/music-for-game-fun-kid-game.mp3'],
         volume: 0.3,
         loop: true,
         html5: true // <--- IMPORTANT: Add this for large music files to help them stream on servers
     }),
-    glitch: new Howl({ src: ['/sounds/pop/bubble-pop.mp3'], volume: 0.5, rate: 2.0 }),
+    glitch: new Howl({ src: ['sounds/pop/bubble-pop.mp3'], volume: 0.5, rate: 2.0 }),
 
     // Helper to stop all music
     stopAll: () => {
